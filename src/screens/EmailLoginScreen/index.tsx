c7f4cb9db04b03c4c4298e2994349f2f3728a467
Nik Ioannou <nikolasgioannou@gmail.com>
2024-04-24 17:14:20 -0400

    add input

--- src/screens/EmailLoginScreen/index.tsx
+++ src/screens/EmailLoginScreen/index.tsx
@@ -1,9 +1,10 @@
-import { Text, View } from "react-native";
+import { Text, TextInput, View } from "react-native";
 
 export const EmailLoginScreen = () => {
   return (
     <View>
       <Text style={{ backgroundColor: "red" }}>Email login</Text>
+      <TextInput placeholder="here" />
     </View>
   );
 };
